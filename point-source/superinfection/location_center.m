dac7dbdb1929da00da4cc4b9e71e946b4ce8a906
banananana6 <88462332+banananana6@users.noreply.github.com>
2025-10-21 14:44:19 -0400

    Update location_center.m

--- point-source/superinfection/location_center.m
+++ point-source/superinfection/location_center.m
@@ -1,6 +1,6 @@
 % i starts at the center, j varies -- superinfection
 
-output_folder = 'C:\Users\xiaoy\Work\primes\point_source\superinfect\graphs_raw';
+output_folder = ''; % fill in folder
 if ~exist(output_folder, 'dir')
     mkdir(output_folder);
 end
@@ -131,4 +131,5 @@ function A = generate_LA2D(rows, cols, range)
             end
         end
     end
+
 end
